754fe1ef3c1867f1d0519a8fd2b2654f349c9540
DanielOram <danoram1@xtra.co.nz>
2015-08-23 05:12:24 +1200

    Link to local bootstrap.css not working. resorted to external link

--- chat-app-socket-io/index.js
+++ chat-app-socket-io/index.js
@@ -1,6 +1,10 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
+//add static route in server that fetches files from ./node_modules/bootstrap/dist/
+var express = require("express");
+app.use('/scripts', express.static(__dirname + '/node_modules/bootstrap/dist/'));
+
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 	//res.send('<h1>My first message with socket.io!</h1>');
