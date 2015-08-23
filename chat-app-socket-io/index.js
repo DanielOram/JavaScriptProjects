2d31eb507504142db301afc812350920f8cb3c0e
DanielOram <danoram1@xtra.co.nz>
2015-08-24 04:08:45 +1200

    setup socket.io in app and log console whenever user connects

--- chat-app-socket-io/index.js
+++ chat-app-socket-io/index.js
@@ -1,5 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
+//initialises a new instance of socket.io by passing the http object
+var io = require('socket.io')(http);
 
 //add static route in server that fetches files from ./node_modules/bootstrap/dist/
 var express = require("express");
@@ -8,6 +10,11 @@ app.use('/scripts', express.static(__dirname + '/node_modules/bootstrap/dist/'))
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 	//res.send('<h1>My first message with socket.io!</h1>');
+});
+
+//listen on the connection event for incoming sockets, and log to console
+io.on('connection', function(socket){
+	console.log('a user connected');
 });
 
 http.listen(3000, function(){
